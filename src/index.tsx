6a11aa805daa3c7619db92c336329453b5822e02
n0eyes <doll4586633@naver.com>
2022-12-22 16:39:36 +0900

    feature: 에러 페이지 생성

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,12 @@ import ReactDOM from "react-dom/client";
 
 import Router from "@router";
 
+import { GlobalStyle } from "./styles/GlobalStyles";
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <StrictMode>
+    <GlobalStyle />
     <Router />
   </StrictMode>,
 );
